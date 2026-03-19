6002bf1e90f5d6b23b33b3bbcf15bdab2cda2768
mellsh <mellsh0813@gmail.com>
2026-03-19 14:36:52 +0900

    create project file and try to connect database

---
+++ README.md
@@ -0,0 +1,11 @@
+---
+
+# 실행 가이드
+
+---
+
+₩₩₩bash
+
+pip install -r requirement.txt
+
+₩₩₩
---
+++ database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+USERNAME = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASSWD")
+HOST = os.getenv("DB_HOST")
+PORT = os.getenv("DB_PORT")
+DBNAME = os.getenv("DB_NAME") 
+
+DB_URL = f'mysql+pymysql://root:Mellsh@0813@127.0.0.1:3306/teampldb'
+
+class engineconn:
+
+    def __init__(self):
+        self.engine = create_engine(DB_URL, pool_recycle = 500)
+
+    def sessionmaker(self):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        return session
+
+    def connection(self):
+        conn = self.engine.connect()
+        return conn
